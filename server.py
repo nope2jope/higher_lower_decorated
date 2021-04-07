ce4df4995167fdc9b8781e334cc3a96a7139b33b
nope2jope <67401076+nope2jope@users.noreply.github.com>
2021-04-07 11:23:40 -0700

    base functionality implemented
    decorator had been a sticking point

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 from flask import Flask
 import random
 
-WELCOME_MAT = "https://media4.giphy.com/media/j2wwZqlOFQG55VmrWX/giphy.gif?cid=ecf05e47aq6z84i1jl8tvx7j5cwj9xrml5b7sk2ltzk0mwuu&rid=giphy.gif"
-TOO_LOW = 'https://media0.giphy.com/media/SL7BxmyLKrsatbUiU0/giphy.gif?cid=ecf05e478r7bpmj8kbs80mb50dlz3ucroom12th8h0eht2vq&rid=giphy.gif'
-TOO_HIGH = 'https://media0.giphy.com/media/xT9IgJJWlg2MAygRoY/giphy.gif?cid=ecf05e47mpzciymycsh6nmgf4jk0xxuu38h5sy8uapknsgml&rid=giphy.gif'
-JUST_RIGHT = 'https://media0.giphy.com/media/92ekxqtLAvaG4/giphy.gif?cid=ecf05e478r7bpmj8kbs80mb50dlz3ucroom12th8h0eht2vq&rid=giphy.gif'
+WELCOME_MAT = "<img src=https://media4.giphy.com/media/j2wwZqlOFQG55VmrWX/giphy.gif?cid=ecf05e47aq6z84i1jl8tvx7j5cwj9xrml5b7sk2ltzk0mwuu&rid=giphy.gif>"
+TOO_LOW = '<img src=https://media0.giphy.com/media/SL7BxmyLKrsatbUiU0/giphy.gif?cid=ecf05e478r7bpmj8kbs80mb50dlz3ucroom12th8h0eht2vq&rid=giphy.gif>'
+TOO_HIGH = '<img src=https://media0.giphy.com/media/xT9IgJJWlg2MAygRoY/giphy.gif?cid=ecf05e47mpzciymycsh6nmgf4jk0xxuu38h5sy8uapknsgml&rid=giphy.gif>'
+JUST_RIGHT = '<img src=https://media0.giphy.com/media/92ekxqtLAvaG4/giphy.gif?cid=ecf05e478r7bpmj8kbs80mb50dlz3ucroom12th8h0eht2vq&rid=giphy.gif>'
 
 LOWER_LIMIT = 0
 UPPER_LIMIT = 9
@@ -12,59 +12,56 @@ UPPER_LIMIT = 9
 answer = ''
 
 WELCOME_MSG = '<h1>Welcome to Marthas Vineyard!</h1>' \
-             f'<h2>Guess a number between {LOWER_LIMIT} and {UPPER_LIMIT} in the url above.</h2>' \
-HIGH_MSG = f'Too high! Try again.'
-LOW_MSG = f'Too low. (Still goes down smooth.) Try again.'
-RIGHT_MSG = f'Aw yeah! Take a drink.'
+             f'<h2>Guess a number between {LOWER_LIMIT} and {UPPER_LIMIT} in the url above.</h2>'
+HIGH_MSG = f'<h2>Too high! Try again.</h2>'
+LOW_MSG = f'<h2>Too low. (Still goes down smooth.) Try again.</h2>'
+RIGHT_MSG = f'<h2>Aw yeah! Take a drink.</h2>'
 COLORS = ['#FF7F50', '#000080', '#4682B4', '#008080' '#Q9932CC', '#00BFFF' '#FFD700', '#ADD8E6', '#3CB371', 'tomato']
 
-
-class GameBrain:
-    def __init__(self):
-
-        self.hello = WELCOME_MSG
-        self.low = LOW_MSG
-        self.high = HIGH_MSG
-        self.correct = RIGHT_MSG
+class GameBrain(Flask):
+    def __init__(self, import_name):
+        super().__init__(import_name)
+        # self.hello = WELCOME_MSG
+        # self.low = LOW_MSG
+        # self.high = HIGH_MSG
+        # self.correct = RIGHT_MSG
         self.palette = COLORS
+        self.application = Flask(import_name)
         self.choice = random.randint(LOWER_LIMIT, UPPER_LIMIT)
-        self.application = Flask(import_name=__name__)
 
-    @application.route('/')
-    def say_hi(self):
-        return self.hello, self.choice
+        def colorize(function):
+            def wrap_up(*args, **kwargs):
+                color = random.choice(self.palette)
+                output = function()
+                output = output.replace(f'<h2>', f'<h2 style="color:{color}">')
+                return output
+            return wrap_up
 
-    @application.route('/<int:number>')
-    # @self.colorize
-    def check_answer(self, number):
-        if number == self.choice:
-            return self.correct
-        elif number > self.choice:
-            return self.high
-        elif number < self.choice:
-            return self.low
+        @self.application.route('/')
+        def say_hi():
+            return WELCOME_MSG + WELCOME_MAT
 
-    def colorize(self, function):
-        color = random.choice(self.palette)
-        output = f'<h2 style="color:{color}">{function}</h2>'
-        return output
+        @colorize
+        def too_low():
+            return LOW_MSG + TOO_LOW
 
+        @colorize
+        def too_high():
+            return HIGH_MSG + TOO_HIGH
 
-    # @colorize
-    def youre_right(self):
-       return self.correct
+        @colorize
+        def correct():
+            return RIGHT_MSG + JUST_RIGHT
 
+        @self.application.route('/<int:number>')
+        def check_answer(number):
+            if number == self.choice:
+                return correct()
+            elif number < self.choice:
+                return too_low()
+            elif number > self.choice:
+                return too_high()
 
-    # @colorize
-    # def lowball(self):
-    #     return self.low
-    #
-    # @colorize
-    # def highball(self):
-    #     return self.high
-
-game = GameBrain()
-
-game.colorize(game.youre_right())
+game = GameBrain(import_name=__name__)
 
 game.application.run(debug=True)
